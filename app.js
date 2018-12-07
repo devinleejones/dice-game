9767e40c96581fca0c880b057ae96fe776b3fa5f
Devin Lee-Jones <devinleejones@gmail.com>
2018-12-07 13:36:29 -0800

    Defined a new gameInitializer function that will set the initial game. Defined a new eventListener for our new game button that takes the gameInitializer function as a callback to reset the game when someone wins.

--- app.js
+++ app.js
@@ -9,18 +9,9 @@ GAME RULES:
 
 */
 
-let scores, roundScore, activePlayer, dice
+let scores, roundScore, activePlayer
 
-scores = [0, 0]
-roundScore = 0
-activePlayer = 0
-
-document.querySelector('.dice').style.display = 'none'
-
-document.getElementById('score-0').textContent = '0'
-document.getElementById('score-1').textContent = '0'
-document.getElementById('current-0').textContent = '0'
-document.getElementById('current-1').textContent = '0'
+gameInitializer()
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
   let dice = Math.floor(Math.random() * 6) + 1
@@ -43,7 +34,7 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
   document.querySelector('#score-' + activePlayer).textContent =
     scores[activePlayer]
 
-  if (scores[activePlayer] >= 100) {
+  if (scores[activePlayer] >= 10) {
     document.querySelector('#name-' + activePlayer).textContent = 'WINNER!'
     document.querySelector('.dice').style.display = 'none'
     document
@@ -67,4 +58,26 @@ function nextPlayer() {
   document.querySelector('.player-1-panel').classList.toggle('active')
 
   document.querySelector('.dice').style.display = 'none'
+}
+
+document.querySelector('.btn-new').addEventListener('click', gameInitializer)
+
+function gameInitializer() {
+  scores = [0, 0]
+  activePlayer = 0
+  roundScore = 0
+
+  document.querySelector('.dice').style.display = 'none'
+
+  document.getElementById('score-0').textContent = '0'
+  document.getElementById('score-1').textContent = '0'
+  document.getElementById('current-0').textContent = '0'
+  document.getElementById('current-1').textContent = '0'
+  document.getElementById('name-0').textContent = 'Player 1'
+  document.getElementById('name-1').textContent = 'Player 2'
+  document.querySelector('.player-0-panel').classList.remove('winner')
+  document.querySelector('.player-1-panel').classList.remove('winner')
+  document.querySelector('.player-0-panel').classList.remove('active')
+  document.querySelector('.player-1-panel').classList.remove('active')
+  document.querySelector('.player-0-panel').classList.add('active')
 }
